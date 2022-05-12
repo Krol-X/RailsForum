fb53a56ceb7c8f502be7a09f69a14c19c2033abb
Alex Krol <krolmail@list.ru>
2022-05-12 17:22:58 +0300

    Adding posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,23 @@
 class PostsController < ApplicationController
+  # Public
   def index
     @forum_messages = ForumMessage.all
+  end
+
+  # API
+  def api_new
+    # todo: use model in form?
+    @forum_message = ForumMessage.new get_forum_message(params)
+    if @forum_message.save
+    else
+      # todo: add session error message
+    end
+    redirect_to posts_path
+  end
+  
+  private
+  
+  def get_forum_message(params)
+    params.permit(:author, :text)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  # Public 
   root "main#index"
   get '/posts', to: 'posts#index'
+  
+  # API
+  post '/posts/api', to: 'posts#api_new'
 end
